8732b1a615ac785a45862c48b1503891b331dd46
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-09-22 10:37:51 +0700

    soal pertama

---
+++ soal1.py
@@ -0,0 +1,13 @@
+nilai = int(input("berapa nilai kamu?"))
+
+
+if nilai >= 90 :
+    print("excellent performance")
+elif nilai >= 80 :
+    print("very good performance")
+elif nilai >= 70 :
+    print("good performance") 
+elif nilai >= 60 :
+    print("averaage performance")
+else :
+    print("below average performance")
