9f25b423f89acd6545dbd01c712f47dd03abdded
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-09-22 11:06:02 +0700

    yang baru

--- soal2.py
+++ soal2.py
@@ -2,11 +2,15 @@ a = float(input("Masukkan angka pertama: "))
 b = float(input("Masukkan angka kedua: "))
 c = float(input("Masukkan angka ketiga: "))
 
-if a >= b and a >= c:
+if a > b and a > c:
     terbesar = a
-elif b >= a and b >= c:
+    print("Angka terbesar adalah:", terbesar)
+elif b > a and b > c:
     terbesar = b
-else:
+    print("Angka terbesar adalah:", terbesar)
+elif c > a and c > c:
     terbesar = c
+    print("Angka terbesar adalah:", terbesar)
+else :
+    print("tidak ada nilai terbesar")
 
-print("Angka terbesar adalah:", terbesar)
